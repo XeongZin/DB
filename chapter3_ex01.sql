0def11947abf54bede661a685517ce6211589868
XeongZin <116179987+XeongZin@users.noreply.github.com>
2023-04-03 11:49:34 +0900

    Add files via upload

--- chapter3_ex01.sql
+++ chapter3_ex01.sql
@@ -1,9 +1,28 @@
-select * from book;
+/*도서번호가 1인 도서의 이름*/
+select bookname from book where bookid=1;
 
-select bookname from book where bookid=1;   //(1) 도서번호가 1인 도서의 이름
-select bookname from book where price>=20000;   //(2) 가격이 2만원 이상인 도서의 이름
-select sum(saleprice) from orders where custid=1;   //(3) 박지성의 총 구매액
-select sum(saleprice) from customer orders where customer.custid=orders.custid and customer.name like '박지성';    //(3-2) 박지성의 총 구매액2
-select count(*) from orders where custid=1; //(4)
+/*가격이 20000원 이상인 도서의 이름*/
+select bookname from book where price>=20000;
+
+/*박지성의 총 구매액*/
+select sum(saleprice) from orders where custid=1;
+
+/*박지성의 총 구매액2*/
+SELECT sum(saleprice) FROM Customer, Orders WHERE Customer.custid=Orders.custid AND Customer.name LIKE '박지성';
+
+/**/
+SELECT count(*) FROM Orders WHERE custid=1;
+
+/**/
+SELECT count(DISTINCT publisher) FROM Customer, Orders, Book WHERE (Customer.custid=Orders.custid) AND (Book.bookid=Orders.bookid) AND (Customer.name='박지성');
+
+/**/
+SELECT bookname, price, price-saleprice FROM Customer, Orders, Book WHERE (Customer.custid=Orders.custid) AND (Book.bookid=Orders.bookid) AND (Customer.name='박지성');
+
+/**/
+SELECT bookname FROM Book, Orders, Customer
+MINUS
+SELECT booknmae FROM Book, Orders, Customer
+WHERE (Customer.custid=Orders.custid) AND (Book.bookid=Orders.bookid) AND (Customer.name='박지성');
 
 
